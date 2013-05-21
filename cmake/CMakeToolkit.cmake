6b9d9fa0ffa1acdb31ccee8227f7cd966e863d22
Nicholas Hutchinson <nshutchinson@gmail.com>
2013-05-21 15:40:20 +0100

    Add CTInit()

--- cmake/CMakeToolkit.cmake
+++ cmake/CMakeToolkit.cmake
@@ -1,21 +1,13 @@
 
 include (./CTAddCompilerFlags)
-include (./CTCheckBuildDirectorySanityOrDie)
 include (./CTCheckDecls)
 include (./CTCheckFuncs)
 include (./CTCheckHeaders)
 include (./CTEnableCXX11)
 include (./CTFindLibrary)
 include (./CTGenerateCopyFilesCommandString)
+include (./CTInit)
 include (./CTObjCARCSetEnabled)
-include (./CTSanitiseBuildType)
 include (./CTSearchLibs)
 include (./CTTargetAddLinkerFlags)
 include (./CTTargetAddPackageDependencies)
-
-###
-
-CTCheckBuildDirectorySanityOrDie()
-
-CTSanitiseBuildType()
-message(STATUS "Build type is: ${CMAKE_BUILD_TYPE}")
---
+++ cmake/CTInit.cmake
@@ -0,0 +1,31 @@
+include (CMakeParseArguments)
+include (./CTCheckBuildDirectorySanityOrDie)
+include (./CTSanitiseBuildType)
+
+function (CTInit)
+  set (options
+    ALLOW_EMPTY_BUILD_TYPE
+    ALLOW_IN_SOURCE_BUILDS
+    OSX_ALLOW_UNSAFE_ASSERTMACROS
+  )
+
+  cmake_parse_arguments(args "${options}" "" "" ${ARGV})
+
+  if (NOT args_ALLOW_IN_SOURCE_BUILDS)
+    CTCheckBuildDirectorySanityOrDie()
+  endif ()
+
+  if (NOT args_ALLOW_EMPTY_BUILD_TYPE)
+    CTSanitiseBuildType()
+    message(STATUS "Build type is: ${CMAKE_BUILD_TYPE}")
+  endif ()
+
+  if (NOT args_OSX_ALLOW_UNSAFE_ASSERTMACROS AND 
+      CMAKE_SYSTEM_NAME STREQUAL Darwin)
+  
+    # OS X's AssertMacros.h is a real pain, and defines some macros that
+    # conflict with some common third-party libraries unless you pass this
+    # #define.
+    add_definitions(-D__ASSERT_MACROS_DEFINE_VERSIONS_WITHOUT_UNDERSCORES=0)
+  endif ()
+endfunction ()
